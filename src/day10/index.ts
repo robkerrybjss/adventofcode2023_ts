7a3abf9f0833a0b5284c8ed90b603dec5767f287
robkerrybjss <rob.kerry@bjss.com>
2023-12-11 10:22:03 +0000

    Day 11 - add day 11 solution

---
+++ src/day10/index.ts
@@ -0,0 +1,18 @@
+import {Day} from "../day";
+
+class Day10 extends Day {
+
+    constructor() {
+        super(10);
+    }
+
+    solveForPartOne(input: string): string {
+        return input;
+    }
+
+    solveForPartTwo(input: string): string {
+        return input;
+    }
+}
+
+export default new Day10;
