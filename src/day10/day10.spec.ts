f9ae859599129291365eb475385789704712c05a
Robert Kerry <thisisrobkerry@gmail.com>
2023-12-13 20:07:16 +0000

    Day 10 - Add day 10 pt 1 solution

--- src/day10/day10.spec.ts
+++ src/day10/day10.spec.ts
@@ -2,6 +2,40 @@ import day10 from './index';
 
 describe('On Day 10', () => {
     it(`part1 is identity function`, () => {
-        expect(day10.solveForPartOne('hello')).toBe('hello');
+        expect(day10.solveForPartOne('-L|F7\n' +
+            '7S-7|\n' +
+            'L|7||\n' +
+            '-L-J|\n' +
+            'L|-JF')).toBe('4');
+    })
+    it(`part1 complex is identity function`, () => {
+        expect(day10.solveForPartOne('7-F7-\n' +
+            '.FJ|7\n' +
+            'SJLL7\n' +
+            '|F--J\n' +
+            'LJ.LJ')).toBe('8');
+    })
+    it(`part2 is identity function`, () => {
+        expect(day10.solveForPartTwo('...........\n' +
+            '.S-------7.\n' +
+            '.|F-----7|.\n' +
+            '.||.....||.\n' +
+            '.||.....||.\n' +
+            '.|L-7.F-J|.\n' +
+            '.|..|.|..|.\n' +
+            '.L--J.L--J.\n' +
+            '...........')).toBe('4');
+    })
+    it(`part2 complex is identity function`, () => {
+        expect(day10.solveForPartTwo('.F----7F7F7F7F-7....\n' +
+            '.|F--7||||||||FJ....\n' +
+            '.||.FJ||||||||L7....\n' +
+            'FJL7L7LJLJ||LJ.L-7..\n' +
+            'L--J.L7...LJS7F-7L7.\n' +
+            '....F-J..F7FJ|L7L7L7\n' +
+            '....L7.F7||L7|.L7L7|\n' +
+            '.....|FJLJ|FJ|F7|.LJ\n' +
+            '....FJL-7.||.||||...\n' +
+            '....L---J.LJ.LJLJ...')).toBe('8');
     })
 });
--- src/day10/index.ts
+++ src/day10/index.ts
@@ -7,11 +7,202 @@ class Day10 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        let grid: string[][] = input.split("\n").map(line => line.split(""));
+
+        let start = this.findStart(grid);
+        let x = start.x;
+        let y = start.y;
+        console.log(`starting at ${x}, ${y}`)
+        let dir: string = '';
+
+        let below = grid[y + 1][x];
+        if (below === '|' || below === 'L' || below === 'J') {
+            dir = 'S';
+            y++;
+        }
+        if (!dir) {
+            let above = grid[y - 1][x];
+            if (above === '|' || above === 'F' || above === '7') {
+                dir = 'N';
+                y--;
+            }
+        }
+        if (!dir) {
+            //only possible starting shape left is -, pick E or W arbitrarily
+            dir = 'E'
+            x++;
+        }
+        let path = [start, {x, y}];
+        let steps = 1;
+        while (x !== start.x || y !== start.y) {
+            let deltaX = 0;
+            let deltaY = 0;
+            switch (grid[y][x] + dir) {
+                case '|S':
+                    deltaY = 1;
+                    break;
+                case '|N':
+                    deltaY = -1;
+                    break;
+                case '-E':
+                    deltaX = 1;
+                    break;
+                case '-W':
+                    deltaX = -1;
+                    break;
+                case 'LS':
+                    deltaX = 1;
+                    break;
+                case 'LW':
+                    deltaY = -1;
+                    break;
+                case 'JS':
+                    deltaX = -1;
+                    break;
+                case 'JE':
+                    deltaY = -1;
+                    break;
+                case '7N':
+                    deltaX = -1;
+                    break;
+                case '7E':
+                    deltaY = 1;
+                    break;
+                case 'FN':
+                    deltaX = 1;
+                    break;
+                case 'FW':
+                    deltaY = 1;
+                    break;
+                default:
+                    throw 'unrecognized ' + grid[y][x] + dir;
+            }
+            if (deltaY === 1) {
+                dir = 'S';
+            } else if (deltaY === -1) {
+                dir = 'N';
+            } else if (deltaX === -1) {
+                dir = 'W';
+            } else {
+                dir = 'E';
+            }
+            x += deltaX;
+            y += deltaY;
+            steps++;
+            path.push({x, y});
+        }
+
+        return "" + (steps / 2);
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        let grid: string[][] = input.split("\n").map(line => line.split(""));
+        let newGrid: string[][] = new Array(grid.length).fill(new Array(grid[0].length).fill("."));
+
+        let start = this.findStart(grid);
+        let x = start.x;
+        let y = start.y;
+        console.log(`starting at ${x}, ${y}`)
+        let dir: string = '';
+        newGrid[y][x] = "X";
+
+        let below = grid[y + 1][x];
+        if (below === '|' || below === 'L' || below === 'J') {
+            dir = 'S';
+            y++;
+        }
+        if (!dir) {
+            let above = grid[y - 1][x];
+            if (above === '|' || above === 'F' || above === '7') {
+                dir = 'N';
+                y--;
+            }
+        }
+        if (!dir) {
+            //only possible starting shape left is -, pick E or W arbitrarily
+            dir = 'E'
+            x++;
+        }
+
+        newGrid = newGrid.splice(y,1, newGrid[y].splice(x,1,'X'));
+        let path = [start, {x, y}];
+        let steps = 1;
+        while (x !== start.x || y !== start.y) {
+            let deltaX = 0;
+            let deltaY = 0;
+            switch (grid[y][x] + dir) {
+                case '|S':
+                    deltaY = 1;
+                    break;
+                case '|N':
+                    deltaY = -1;
+                    break;
+                case '-E':
+                    deltaX = 1;
+                    break;
+                case '-W':
+                    deltaX = -1;
+                    break;
+                case 'LS':
+                    deltaX = 1;
+                    break;
+                case 'LW':
+                    deltaY = -1;
+                    break;
+                case 'JS':
+                    deltaX = -1;
+                    break;
+                case 'JE':
+                    deltaY = -1;
+                    break;
+                case '7N':
+                    deltaX = -1;
+                    break;
+                case '7E':
+                    deltaY = 1;
+                    break;
+                case 'FN':
+                    deltaX = 1;
+                    break;
+                case 'FW':
+                    deltaY = 1;
+                    break;
+                default:
+                    throw 'unrecognized ' + grid[y][x] + dir;
+            }
+            if (deltaY === 1) {
+                dir = 'S';
+            } else if (deltaY === -1) {
+                dir = 'N';
+            } else if (deltaX === -1) {
+                dir = 'W';
+            } else {
+                dir = 'E';
+            }
+            x += deltaX;
+            y += deltaY;
+            steps++;
+            path.push({x, y});
+            newGrid = newGrid.splice(y,1, newGrid[y].splice(x,1,'X'));
+        }
+
+        console.log(newGrid)
+
+        return "" + (steps / 2);
+    }
+
+    private findStart(grid: string[][]): { x: number, y: number } {
+        let start = {x: 0, y: 0}
+        grid.forEach((line, yIndex) => {
+            line.forEach((char, xIndex) => {
+                if (char === "S") {
+                    console.log(`Found S at ${xIndex}, ${yIndex}`)
+                    start.x = xIndex
+                    start.y = yIndex
+                }
+            })
+        })
+        return start;
     }
 }
 
